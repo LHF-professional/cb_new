0cf9dffaef71717539c49a1d204ada1be290c767
LHF-professional <127985960+LHF-professional@users.noreply.github.com>
2023-03-29 08:39:07 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,61 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, request, jsonify
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/run-script', methods=['POST'])
-def run_script():
-    script_name = request.form['script']
-    try:
-        os.system(f'python scripts/{script_name}')
-        success = True
-    except:
-        success = False
-    if success:
-        if script_name == 'script2.py':
-            images = os.listdir('static/images')
-            if images:
-                return {'success': True, 'images': images}
-            else:
-                return {'success': True}
-        else:
-            return {'success': True}
-    else:
-        return {'success': False}
+@app.route('/button1', methods=['POST'])
+def button1():
+    if request.method == 'POST':
+        # Disable the button
+        button_id = request.form['button_id']
+        response = {'button_id': button_id}
+        return jsonify(response)
+
+@app.route('/button2', methods=['POST'])
+def button2():
+    if request.method == 'POST':
+        # Disable the button
+        button_id = request.form['button_id']
+        response = {'button_id': button_id}
+
+        # Run script
+        time.sleep(5)
+
+        # Display images
+        image_urls = ['image1.jpg', 'image2.jpg', 'image3.jpg']
+        return jsonify({'image_urls': image_urls})
+
+@app.route('/button3', methods=['POST'])
+def button3():
+    if request.method == 'POST':
+        # Disable the button
+        button_id = request.form['button_id']
+        response = {'button_id': button_id}
+
+        # Run script
+        time.sleep(5)
+
+        # Display success message
+        message = 'Button 3 executed successfully'
+        return jsonify({'message': message})
+
+@app.route('/button4', methods=['POST'])
+def button4():
+    if request.method == 'POST':
+        # Disable the button
+        button_id = request.form['button_id']
+        response = {'button_id': button_id}
+
+        # Run script
+        time.sleep(5)
+
+        # Display error message
+        message = 'An error occurred while executing Button 4'
+        return jsonify({'message': message})
 
 if __name__ == '__main__':
     app.run(debug=True)
